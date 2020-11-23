9cbddbb82c22ae8408e20183853b2e38842ee711
Jialong ZHANG <JZhang182@slb.com>
2020-11-23 19:40:08 +0800

    refactor: move files to folder

---
+++ PetApi/Models/PetUpdateModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PetApi
+{
+    public class PetUpdateModel
+    {
+        public PetUpdateModel()
+        {
+        }
+
+        public PetUpdateModel(string name, int price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public string Name { get; set; }
+        public int Price { get; set; }
+    }
+}
